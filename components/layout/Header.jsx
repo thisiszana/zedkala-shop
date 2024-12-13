ed1fc24ff004e92b17cbede97b95eec30901189e
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-12-13 22:40:01 +0330

    Completing the ui reload from the header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -4,6 +4,7 @@ import MobileNav from "./MobileNav";
 import Image from "next/image";
 import { images } from "@/constants";
 import SearchBox from "../shared/SearchBox";
+import { ShoppingCart, User } from "../icons/Icons";
 
 export default function Header() {
   return (
@@ -15,9 +16,11 @@ export default function Header() {
             <Image src={images.home_logo} width={100} height={100} alt="لوگو" />
           </Link>
           <SearchBox />
-          <DesktopNav />
         </div>
-        <div></div>
+        <div className="flex items-center gap-3">
+          <User />
+          <ShoppingCart />
+        </div>
       </div>
     </header>
   );
